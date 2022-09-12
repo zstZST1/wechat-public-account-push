7ddd883e754fbe496710779bcc38b2a721fd6c07
zstZST1 <113335616+zstZST1@users.noreply.github.com>
2022-09-12 16:59:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx27b1d64fe71885bd",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "ae1d69185b53112cda5670c2b86834a8",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -25,9 +25,9 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "wx27b1d64fe71885bd",
+      id: "oIhbX5vECiYMY6uk30cFVZwNDrcE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "ae1d69185b53112cda5670c2b86834a8",
+      useTemplateId: "BTtNcBST3ifLpM-taM5WOQFSBa4eTvTbXlEl6VYJhSc",
       // 所在省份
       province: "黑龙江",
       // 所在城市
